720ed2600f91529c82e15e03b5604bb18dd0c1c0
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-11-27 20:38:54 +0100

    Création de la première migration du projet - V1

---
+++ migrations/Version20241127193744.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241127193744 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name_category VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, last_name VARCHAR(50) NOT NULL, first_name VARCHAR(50) DEFAULT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(25) NOT NULL, username VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, city VARCHAR(50) NOT NULL, postal_code VARCHAR(10) NOT NULL, birth_date DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', rpps_number VARCHAR(11) DEFAULT NULL, customer_type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE delivery (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, employee_id INT NOT NULL, delivery_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', quantity INT NOT NULL, delivery_address VARCHAR(255) NOT NULL, postal_code VARCHAR(10) NOT NULL, city VARCHAR(50) NOT NULL, INDEX IDX_3781EC109395C3F3 (customer_id), INDEX IDX_3781EC108C03F15C (employee_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, last_name VARCHAR(50) NOT NULL, first_name VARCHAR(50) NOT NULL, phone VARCHAR(25) NOT NULL, email VARCHAR(255) NOT NULL, username VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE inventory (id INT AUTO_INCREMENT NOT NULL, employee_id INT NOT NULL, stock_id INT NOT NULL, inventory_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_B12D4A368C03F15C (employee_id), INDEX IDX_B12D4A36DCD6110 (stock_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, customer_id INT DEFAULT NULL, order_number INT NOT NULL, order_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_F52993989395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE order_line (id INT AUTO_INCREMENT NOT NULL, order_id INT NOT NULL, product_id INT NOT NULL, quantity INT NOT NULL, unit_price DOUBLE PRECISION NOT NULL, INDEX IDX_9CE58EE18D9F6D38 (order_id), INDEX IDX_9CE58EE14584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, stock_id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, price DOUBLE PRECISION NOT NULL, images LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\', expiration_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_D34A04AD12469DE2 (category_id), INDEX IDX_D34A04ADDCD6110 (stock_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stock (id INT AUTO_INCREMENT NOT NULL, quantity INT NOT NULL, alert INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE supplier (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, address VARCHAR(255) NOT NULL, postal_code VARCHAR(10) NOT NULL, city VARCHAR(50) NOT NULL, phone VARCHAR(25) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE supply (id INT AUTO_INCREMENT NOT NULL, supplier_id INT NOT NULL, employee_id INT NOT NULL, supply_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_D219948C2ADD6D8C (supplier_id), INDEX IDX_D219948C8C03F15C (employee_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE supply_line (id INT AUTO_INCREMENT NOT NULL, supply_id INT NOT NULL, product_id INT NOT NULL, unit_price DOUBLE PRECISION NOT NULL, quantity INT NOT NULL, INDEX IDX_6AFCD0EFFF28C0D8 (supply_id), INDEX IDX_6AFCD0EF4584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE delivery ADD CONSTRAINT FK_3781EC109395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
+        $this->addSql('ALTER TABLE delivery ADD CONSTRAINT FK_3781EC108C03F15C FOREIGN KEY (employee_id) REFERENCES employee (id)');
+        $this->addSql('ALTER TABLE inventory ADD CONSTRAINT FK_B12D4A368C03F15C FOREIGN KEY (employee_id) REFERENCES employee (id)');
+        $this->addSql('ALTER TABLE inventory ADD CONSTRAINT FK_B12D4A36DCD6110 FOREIGN KEY (stock_id) REFERENCES stock (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F52993989395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
+        $this->addSql('ALTER TABLE order_line ADD CONSTRAINT FK_9CE58EE18D9F6D38 FOREIGN KEY (order_id) REFERENCES `order` (id)');
+        $this->addSql('ALTER TABLE order_line ADD CONSTRAINT FK_9CE58EE14584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADDCD6110 FOREIGN KEY (stock_id) REFERENCES stock (id)');
+        $this->addSql('ALTER TABLE supply ADD CONSTRAINT FK_D219948C2ADD6D8C FOREIGN KEY (supplier_id) REFERENCES supplier (id)');
+        $this->addSql('ALTER TABLE supply ADD CONSTRAINT FK_D219948C8C03F15C FOREIGN KEY (employee_id) REFERENCES employee (id)');
+        $this->addSql('ALTER TABLE supply_line ADD CONSTRAINT FK_6AFCD0EFFF28C0D8 FOREIGN KEY (supply_id) REFERENCES supply (id)');
+        $this->addSql('ALTER TABLE supply_line ADD CONSTRAINT FK_6AFCD0EF4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE delivery DROP FOREIGN KEY FK_3781EC109395C3F3');
+        $this->addSql('ALTER TABLE delivery DROP FOREIGN KEY FK_3781EC108C03F15C');
+        $this->addSql('ALTER TABLE inventory DROP FOREIGN KEY FK_B12D4A368C03F15C');
+        $this->addSql('ALTER TABLE inventory DROP FOREIGN KEY FK_B12D4A36DCD6110');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F52993989395C3F3');
+        $this->addSql('ALTER TABLE order_line DROP FOREIGN KEY FK_9CE58EE18D9F6D38');
+        $this->addSql('ALTER TABLE order_line DROP FOREIGN KEY FK_9CE58EE14584665A');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD12469DE2');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04ADDCD6110');
+        $this->addSql('ALTER TABLE supply DROP FOREIGN KEY FK_D219948C2ADD6D8C');
+        $this->addSql('ALTER TABLE supply DROP FOREIGN KEY FK_D219948C8C03F15C');
+        $this->addSql('ALTER TABLE supply_line DROP FOREIGN KEY FK_6AFCD0EFFF28C0D8');
+        $this->addSql('ALTER TABLE supply_line DROP FOREIGN KEY FK_6AFCD0EF4584665A');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE customer');
+        $this->addSql('DROP TABLE delivery');
+        $this->addSql('DROP TABLE employee');
+        $this->addSql('DROP TABLE inventory');
+        $this->addSql('DROP TABLE `order`');
+        $this->addSql('DROP TABLE order_line');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE stock');
+        $this->addSql('DROP TABLE supplier');
+        $this->addSql('DROP TABLE supply');
+        $this->addSql('DROP TABLE supply_line');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
