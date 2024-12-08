720cace9ca8f0fc8a248942e223c34781f23ed97
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2024-12-08 19:14:34 +0100

    Modification du controller pour récupérer des produits similaires à partir de la catégorie du produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,9 +19,13 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/{id}', name: 'product_show', requirements: ['id' => '\d+'])]
-    public function show(Product $product): Response
+    public function show(Product $product, ProductRepository $productRepository): Response
     {
-        return $this->render('product/show.html.twig', ['product' => $product]);
+        $similarProducts = $productRepository->findBy(['category' => $product->getCategory()->getId()], null, 4);
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+            'similarProducts' => $similarProducts,
+        ]);
     }
 
 }
