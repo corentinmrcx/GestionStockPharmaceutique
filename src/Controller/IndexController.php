acb9c8ed67a10bf2bb5a1f79e13fd54d9e3a9700
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2024-12-09 21:09:55 +0100

    Affichage des produits conseillés sur la page d'accueil

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $recommendedProducts = $productRepository->findBy(['isRecommended' => true]);
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'recommendedProducts' => $recommendedProducts,
         ]);
     }
 }
