2140ab28c1edf5eeb612fdca85fc5d9602a543ac
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2024-12-10 13:18:49 +0100

    Ajout du jeu d'essai (fixtures, forge) pour Stock

---
+++ src/Factory/StockFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Stock;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Stock>
+ */
+final class StockFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Stock::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'alert' => self::faker()->randomNumber(),
+            'quantity' => self::faker()->randomNumber(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Stock $stock): void {})
+        ;
+    }
+}
