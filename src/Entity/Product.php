9087656fc14668527338acd8a65e94f096cf7506
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2024-12-10 20:58:30 +0100

    Modification du nom du setter de "isRecommended"

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -239,7 +239,7 @@ class Product
         return $this->isRecommended;
     }
 
-    public function setRecommended(bool $isRecommended): static
+    public function setIsRecommended(bool $isRecommended): static
     {
         $this->isRecommended = $isRecommended;
 
