d6f880df2f0c20dc0b7f92f6328aefcfec3ed643
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2024-12-10 13:02:59 +0100

    Ajout de Zenstruck Foundry pour la génération de données factices

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
