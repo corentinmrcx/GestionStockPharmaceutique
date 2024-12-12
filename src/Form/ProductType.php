ed27ed8d4d6d77f223e067ce16fe6624e150e9a9
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-12 19:40:15 +0100

    Modification du formulaire ProductType avec les types adaptés et les contraintes nécessaires.

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -9,6 +9,10 @@ use App\Entity\Stock;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,14 +21,21 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
-            ->add('images')
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('price', MoneyType::class, [
+                'currency' => 'EUR'
+            ])
+            ->add('images', FileType::class, [
+                'multiple' => true,
+                'required' => false,
+            ])
             ->add('expirationDate', null, [
                 'widget' => 'single_text',
             ])
-            ->add('reference')
+            ->add('reference', TextType::class)
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'nameCategory',
