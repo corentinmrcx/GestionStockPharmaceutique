e337d9d9ce767e2de1838493b98c6651289ebd7e
marc0237 <corentin.marcoux@etudiant.univ-reims.fr>
2024-12-17 21:21:49 +0100

    Ajout de la nouvelle migration de la base

---
+++ migrations/Version20241217201529.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241217201529 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product ADD image_name VARCHAR(255) NOT NULL, ADD updated_at DATETIME DEFAULT NULL, DROP images');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product ADD images LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\', DROP image_name, DROP updated_at');
+    }
+}
