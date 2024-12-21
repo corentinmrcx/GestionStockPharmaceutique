a8987532339a822bc984353900f3b3160ce6222f
corentinmrcx <corentinmarcoux51@gmail.com>
2024-12-21 12:01:56 +0100

    Installation du composant VichUploaderBundle pour gérer les images des produits

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
