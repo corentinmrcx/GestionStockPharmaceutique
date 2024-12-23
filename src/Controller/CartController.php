358a3271aba4b24a195f6b47710e84849d4ed779
RomsLob1 <rolob08@gmail.com>
2024-12-23 12:13:07 +0100

    Ajout de l'authentification pour consulter le panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,7 +6,9 @@ use App\Repository\CartLineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'app_cart_index', methods : ['GET'])]
