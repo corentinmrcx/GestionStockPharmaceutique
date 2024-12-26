90acac340193705c0d888a400c5b7627a2630ffe
marc0237 <corentinmarcoux51@gmail.com>
2024-12-26 14:03:16 +0100

    Création du ProductCrudController avec quelques configurations

---
+++ src/Controller/Manager/ProductCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Manager;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+                ->setEntityLabelInPlural('Produits')
+                ->setEntityLabelInSingular('Produit')
+                ->setPageTitle(Crud::PAGE_INDEX, 'Pharmacie - Gestion des produits')
+                ->setPaginatorPageSize(20);
+    }
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
