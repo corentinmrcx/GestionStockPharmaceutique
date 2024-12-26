5265e6b151524d14d24313350ee606ef608475a1
marc0237 <corentinmarcoux51@gmail.com>
2024-12-26 13:27:36 +0100

    Création du Dashboard pour les gestionnaires : - Creation du controller - Création du template index

---
+++ src/Controller/Manager/ManagerDashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Manager;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ManagerDashboardController extends AbstractDashboardController
+{
+    #[Route('/manager', name: 'app_manager_index')]
+    public function index(): Response
+    {
+        return $this->render('manager/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Sae3 01');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
