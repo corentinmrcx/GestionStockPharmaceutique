fed9dfd80f8a783c1e3fc69f97723c681fb51d26
RomsLob1 <rolob08@gmail.com>
2024-12-26 17:43:50 +0100

    Ajout du panier dans la page catalogue du site (/product)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,10 +7,12 @@ use App\Entity\CartLine;
 use App\Entity\Product;
 use App\Form\CartLineType;
 use App\Repository\CartLineRepository;
+use App\Repository\CartRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
@@ -28,10 +30,59 @@ class ProductController extends AbstractController
             12
         );
 
+        foreach ($products as $product) {
+            $form = $this->createFormBuilder()
+                ->setAction($this->generateUrl('app_product_add', ['id' => $product->getId()]))
+                ->setMethod('POST')
+                ->add('add', SubmitType::class, [
+                    'label' => 'Ajoutez au panier',
+                    'attr' => [
+                        'class' => 'btn-custom',
+                        'style' => 'margin-top: 10px; background-color: #8bd59e; border-color: #28a745;',
+                    ],
+                ])
+                ->getForm();
+            $addCart[$product->getId()] = $form->createView();
+        }
+
         return $this->render('product/index.html.twig', [
             'products' => $products,
             'search' => $search,
+            'addCart' => $addCart,
         ]);
+    }
+
+    #[Route('/product/addCartIndex/{id}', name: 'app_product_add', methods: ['POST'])]
+    public function addCartIndex(CartRepository $cartRepository, EntityManagerInterface $entityManager, CartLineRepository $cartLineRepository, Product $product): Response
+    {
+        $user = $this->getUser();
+        $cart = $cartRepository->findOneBy(['user' => $user]);
+
+        if (!$cart) {
+            $cart = new Cart();
+            $cart->setUser($user);
+            $entityManager->persist($cart);
+            $entityManager->flush();
+        }
+
+        $cartLine = $cartLineRepository->findOneBy(['cart' => $cart, 'product' => $product]);
+        if ($cartLine) {
+            $currentQuantity = $cartLine->getQuantity();
+        }
+
+        if ($cartLine) {
+            $cartLine->setQuantity($currentQuantity + 1);
+        } else {
+            $cartLine = new CartLine();
+            $cartLine->setCart($cart);
+            $cartLine->setProduct($product);
+            $cartLine->setQuantity(1);
+        }
+
+        $entityManager->persist($cartLine);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_product');
     }
 
     #[Route('/product/{id}', name: 'cart_add_show', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
