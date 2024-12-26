71c535fce1bb1c4006814470f3f488e7e208ce65
marc0237 <corentinmarcoux51@gmail.com>
2024-12-26 14:02:55 +0100

    Ajout d'un item dans le menu pour modifier les produits

--- src/Controller/Manager/ManagerDashboardController.php
+++ src/Controller/Manager/ManagerDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Manager;
 
+use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -26,7 +27,7 @@ class ManagerDashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
+        yield MenuItem::linkToCrud('Produits', 'fas fa-capsules', Product::class);
     }
 }
