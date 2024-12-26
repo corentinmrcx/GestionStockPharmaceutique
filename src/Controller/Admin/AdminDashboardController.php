ecec4fa38efa1bf90cb9c12f681a173c1003a133
marc0237 <corentinmarcoux51@gmail.com>
2024-12-26 20:48:48 +0100

    Ajout du tableau de board pour les administrateurs + le template associé

---
+++ src/Controller/Admin/AdminDashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+#[Route('/admin')]
+class AdminDashboardController extends AbstractDashboardController
+{
+    #[Route('/', name: 'app_admin_index')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('DashBoard - Administrateur')
+            ->setDefaultColorScheme('dark')
+            ->renderContentMaximized();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
