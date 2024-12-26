b8906ccc10f0de33450d1add35218ef07d64752b
marc0237 <corentinmarcoux51@gmail.com>
2024-12-26 14:42:14 +0100

    Ajout des champs nécessaire pour le ProductCrudController

--- src/Controller/Manager/ProductCrudController.php
+++ src/Controller/Manager/ProductCrudController.php
@@ -5,8 +5,11 @@ namespace App\Controller\Manager;
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProductCrudController extends AbstractCrudController
@@ -24,14 +27,30 @@ class ProductCrudController extends AbstractCrudController
                 ->setPageTitle(Crud::PAGE_INDEX, 'Pharmacie - Gestion des produits')
                 ->setPaginatorPageSize(20);
     }
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('reference', 'Référence'),
+            TextField::new('name', 'Nom du produit'),
+            TextField::new('description', 'Description')
+                ->hideOnIndex(),
+            MoneyField::new('price', 'Prix')->setCurrency('EUR'),
+            ImageField::new('imageName', 'Image')
+                ->setBasePath('images/products')
+                ->setUploadDir('public/images/products')
+                ->setRequired(false),
+            AssociationField::new('category', 'Catégorie')
+                ->setFormTypeOption('choice_label', 'name_category')
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getCategory()->getNameCategory();
+                }),
+            AssociationField::new('brand', 'Marque')
+                ->setFormTypeOption('choice_label', 'name')
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getBrand()->getName();
+                }),
+            DateField::new('expirationDate', 'Date d\'expiration'),
         ];
     }
-    */
 }
