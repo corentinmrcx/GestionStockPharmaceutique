6fbaf32a4961409e5bc142ce5a07d793714cd89b
marc0237 <corentinmarcoux51@gmail.com>
2024-12-27 15:05:13 +0100

    Ajout du CRUD pour les commandes

---
+++ src/Controller/Admin/OrderCrudController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Commandes')
+            ->setEntityLabelInSingular('Commande')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Pharmacie - Gestion des commandes')
+            ->setPaginatorPageSize(20);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id', 'Numéro de la commande'),
+            DateField::new('orderDate', 'Date de la commande'),
+        ];
+    }
+}
