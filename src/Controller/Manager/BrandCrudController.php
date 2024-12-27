115a0ed2565a635ae35ae3804ea629bf2a974390
marc0237 <corentinmarcoux51@gmail.com>
2024-12-27 14:34:44 +0100

    Ajout du CRUD pour les marques

---
+++ src/Controller/Manager/BrandCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Manager;
+
+use App\Entity\Brand;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class BrandCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Brand::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Marques')
+            ->setEntityLabelInSingular('Marque')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Pharmacie - Gestion des marques')
+            ->setPaginatorPageSize(20);
+    }
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'Nom de la marque'),
+        ];
+    }
+}
