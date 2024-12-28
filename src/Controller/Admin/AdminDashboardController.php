8d4b7bbb6a550101447522ab3de3a790fa766caa
marc0237 <corentinmarcoux51@gmail.com>
2024-12-28 22:38:49 +0100

    Restriction de l'accès aux routes "/admin" et "/manager" pour les roles concernés

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -10,9 +10,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_ADMIN')]
 class AdminDashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'app_admin_index')]
--- src/Controller/Manager/ManagerDashboardController.php
+++ src/Controller/Manager/ManagerDashboardController.php
@@ -11,9 +11,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_MANAGER')]
 class ManagerDashboardController extends AbstractDashboardController
 {
     #[Route('/manager', name: 'app_manager_index')]
