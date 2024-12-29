f1a8a5a42f2dfdc18548586b616b48514621498c
marc0237 <corentinmarcoux51@gmail.com>
2024-12-29 20:22:48 +0100

    Ajout de la méthode pour éditer le profile d'in utilisateur

--- src/Controller/UserDashboardController.php
+++ src/Controller/UserDashboardController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,7 +16,7 @@ class UserDashboardController extends AbstractController
     public function index(): Response
     {
         $user = $this->getUser();
-        if (!$user){
+        if (!$user) {
             return $this->redirectToRoute('app_login');
         }
 
@@ -27,6 +31,33 @@ class UserDashboardController extends AbstractController
 
         return $this->render('user_dashboard/index.html.twig', [
             'role' => $role,
+        ]);
+    }
+
+    #[Route('/editprofile/profile/{id}', name: 'app_user_edit')]
+    public function edit(User $user, Request $request, EntityManagerInterface $manager): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($this->getUser() !== $user) {
+            return $this->redirectToRoute('app_index');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_user_dashboard');
+        }
+
+        return $this->render('user_dashboard/editProfile.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
