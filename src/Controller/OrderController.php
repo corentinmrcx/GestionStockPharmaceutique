766480f5bd0d9ecc13f0a19573aaea80314c84a6
RomsLob1 <rolob08@gmail.com>
2025-01-01 19:22:44 +0100

    Ajout de la redirection vers les commandes dans le index, mise en page de la description des commandes

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
+use App\Repository\OrderLineRepository;
 use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\Order;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class OrderController extends AbstractController
 {
-    #[Route('/orders', name: 'app_order')]
+    #[Route('/order', name: 'app_order')]
     public function index(OrderRepository $orderRepository): Response
     {
         $user = $this->getUser();
@@ -21,6 +24,18 @@ class OrderController extends AbstractController
 
         return $this->render('order/index.html.twig', [
             'controller_name' => 'OrderController',
+            'orders' => $orders,
+        ]);
+    }
+    #[Route('/order/{id}', name: 'app_order_show')]
+    public function show(int $id, OrderRepository $orderRepository): Response
+    {
+        $orders = $orderRepository->find($id);
+
+        if (!$orders) {
+            throw $this->createNotFoundException('Order not found');
+        }
+        return $this->render('order/show.html.twig', [
             'orders' => $orders,
         ]);
     }
