01595d02d361a86afa510c61d3399f2cdc75d19d
RomsLob1 <rolob08@gmail.com>
2025-01-01 17:25:14 +0100

    Modification pour mettre en place l'affichage de l'historique des commandes d'un utilisateur (en suspend, problème d'entité non existante)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,23 +2,26 @@
 
 namespace App\Controller;
 
-use App\Repository\OrderLineRepository;
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'app_order')]
-    public function index(OrderLineRepository $orderLineRepository): Response
+    #[Route('/orders', name: 'app_order')]
+    public function index(OrderRepository $orderRepository): Response
     {
         $user = $this->getUser();
         if (!$user) {
             return $this->redirectToRoute('app_login');
         }
 
+        $orders = $orderRepository->findByUser($user->getId());
+
         return $this->render('order/index.html.twig', [
             'controller_name' => 'OrderController',
+            'orders' => $orders,
         ]);
     }
 }
