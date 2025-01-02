dbda22b06cff8d60f7970cc68693b520ca3486f6
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-02 16:08:35 +0100

    Ajout d'un test vérifiant l'affichage d'un produit et ses caractéristiques

--- tests/Controller/Product/IndexCest.php
+++ tests/Controller/Product/IndexCest.php
@@ -25,4 +25,24 @@ class IndexCest
         $I->see('Notre catalogue de produits', 'h1');
         $I->seeNumberOfElements('.product-card a', 5);
     }
+
+    public function ProductDisplaysCorrectDetails(ControllerTester $I): void
+    {
+        $category = CategoryFactory::createOne();
+        $brand = BrandFactory::createOne();
+
+        $product = ProductFactory::createOne([
+            'name' => 'Produit Test',
+            'description' => 'Description test produit',
+            'price' => 49.99,
+            'category' => $category,
+            'brand' => $brand,
+        ]);
+
+        $I->amOnPage('/product');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->see('Produit Test', '.product-card h5');
+        $I->see('Description test produit', '.product-card p');
+        $I->see('49,99 €', '.product-card .price');
+    }
 }
