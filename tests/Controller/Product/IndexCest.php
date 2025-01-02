f4f68ff537243368f59a4aa8a2eb2f269d73ed48
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-02 16:48:45 +0100

    Ajout d'un test vérifiant que la pagination fonctionne correctement

--- tests/Controller/Product/IndexCest.php
+++ tests/Controller/Product/IndexCest.php
@@ -62,4 +62,29 @@ class IndexCest
         $I->seeResponseCodeIsSuccessful(200);
         $I->seeCurrentRouteIs('cart_add_show', ['id' => $product->getId()]);
     }
+
+    public function clickOnNextAndPreviousPaginationLinksWorkCorrectly(ControllerTester $I): void
+    {
+        $category = CategoryFactory::createOne();
+        $brand = BrandFactory::createOne();
+
+        ProductFactory::createMany(20, [
+            'category' => $category,
+            'brand' => $brand,
+        ]);
+
+        $I->amOnPage('/product?page=1');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeNumberOfElements('.product-card', 12);
+
+        $I->click('a[rel="next"]');
+        $I->seeInCurrentUrl('page=2');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeNumberOfElements('.product-card', 8);
+
+        $I->click('a[rel="prev"]');
+        $I->seeInCurrentUrl('page=1');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeNumberOfElements('.product-card', 12);
+    }
 }
