856a97442bbaeb855695cda88bbe9a44c9b37330
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 14:16:11 +0100

    Ajout d'un test qui vérifie l'affichage des produits recommandés sur la page d'accueil

---
+++ tests/Controller/Index/IndexCest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Controller\Index;
+
+use App\Factory\BrandFactory;
+use App\Factory\CategoryFactory;
+use App\Factory\ProductFactory;
+use App\Tests\Support\ControllerTester;
+
+class IndexCest
+{
+    public function homePageDisplaysRecommendedProducts(ControllerTester $I): void
+    {
+        $category = CategoryFactory::createOne();
+        $brand = BrandFactory::createOne();
+        ProductFactory::createMany(10, [
+            'isRecommended' => true,
+            'category' => $category,
+            'brand' => $brand,
+        ]);
+
+        $I->amOnPage('/');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->see('Nos produits conseils du mois', 'h1');
+        $I->seeNumberOfElements('.product-card', 6);
+
+        $product = ProductFactory::random();
+        $I->see($product->getName(), '.product-card h5');
+        $I->see(number_format($product->getPrice(), 2, ',', ' ').' €', '.product-card .price');
+    }
+}
