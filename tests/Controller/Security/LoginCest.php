32452f68ebb8cf3283b5b5ddce6fd41e222fda42
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 18:16:57 +0100

    Ajout du test qui vérifie qu'il n'est pas possible de se connecter avec des identifiants invalides

--- tests/Controller/Security/LoginCest.php
+++ tests/Controller/Security/LoginCest.php
@@ -19,4 +19,15 @@ class LoginCest
         $I->seeResponseCodeIsSuccessful(200);
     }
 
+    public function UserCannotLoginWithInvalidCredentials(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['email' => 'user@example.com', 'password' => 'test']);
+
+        $I->amOnPage('/login');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->fillField('email', 'wrong@example.com');
+        $I->fillField('password', 'wrongpassword');
+        $I->click('Connexion');
+        $I->see('Identifiants invalides');
+    }
 }
