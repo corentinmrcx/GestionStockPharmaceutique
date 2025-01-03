bb61ce2c535eec496ca209188d7985ea47995a5a
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 16:33:06 +0100

    Ajout du test pour vérifier les erreurs lors de l'édition du profil

--- tests/Controller/UserDashboard/EditCest.php
+++ tests/Controller/UserDashboard/EditCest.php
@@ -32,4 +32,41 @@ class EditCest
         $I->seeResponseCodeIsSuccessful(200);
         $I->seeCurrentRouteIs('app_user_dashboard');
     }
+
+    public function editProfileDisplaysForInvalidData(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['email' => 'user@example.com', 'password' => 'password']);
+
+        $I->amOnPage('/login');
+        $I->fillField('email', $user->getEmail());
+        $I->fillField('password', 'password');
+        $I->click('Connexion');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->amOnPage('/editprofile/profile/'.$user->getId());
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->fillField('Prénom *', 'A');
+        $I->fillField('Nom *', 'A');
+        $I->click('Modifier mes informations');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeElement('//div[contains(., "Le prénom ne doit pas être inférieur à 2 caractères.")]');
+        $I->seeElement('//div[contains(., "Le nom ne doit pas être inférieur à 2 caractères.")]');
+
+        $I->fillField('Adresse', str_repeat('A', 256));
+        $I->click('Modifier mes informations');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeElement('//div[contains(., "L\'adresse ne peut pas dépasser 255 caractères.")]');
+
+        $I->fillField('Ville', str_repeat('A', 256));
+        $I->click('Modifier mes informations');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeElement('//div[contains(., "La ville ne peut pas dépasser 255 caractères.")]');
+
+        $I->fillField('Numéro de téléphone *', '123456789');
+        $I->click('Modifier mes informations');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeElement('//div[contains(., "Le numéro de téléphone doit contenir 10 chiffres.")]');
+    }
+
 }
