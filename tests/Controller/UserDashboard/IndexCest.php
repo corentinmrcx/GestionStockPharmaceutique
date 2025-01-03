6e49753f8ef3cf72bfb4b11f2b9926f4aa51721a
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 15:51:50 +0100

    Ajout du test qui vérifie le bon fonctionnement du dashboard de l'admin

--- tests/Controller/UserDashboard/IndexCest.php
+++ tests/Controller/UserDashboard/IndexCest.php
@@ -46,6 +46,7 @@ class IndexCest
         $I->seeResponseCodeIsSuccessful(200);
         $I->see('Se déconnecter', 'a');
     }
+
     public function dashboardDisplaysForRoleManager(ControllerTester $I): void
     {
         $manager = UserFactory::createOne(['email' => 'manager@example.com', 'password' => 'password', 'roles' => ['ROLE_MANAGER']]);
@@ -77,6 +78,44 @@ class IndexCest
         $I->click('Éditer mon Mot de passe');
         $I->seeResponseCodeIsSuccessful(200);
         $I->seeCurrentRouteIs('app_user_edit_password', ['id' => $manager->getId()]);
+
+        $I->amOnPage('/dashboard');
+        $I->click('Déconnexion');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->see('Se déconnecter', 'a');
+    }
+
+    public function dashboardDisplaysForRoleAdmin(ControllerTester $I): void
+    {
+        $admin = UserFactory::createOne(['email' => 'admin@example.com', 'password' => 'password', 'roles' => ['ROLE_ADMIN']]);
+
+        $I->amOnPage('/login');
+        $I->fillField('email', $admin->getEmail());
+        $I->fillField('password', 'password');
+        $I->click('Connexion');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->amOnPage('/dashboard');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->see('DashBoard Administrateur', '.dashboard-card h5');
+        $I->see('Éditer mon Profil', '.dashboard-card h5');
+        $I->see('Éditer mon Mot de passe', '.dashboard-card h5');
+        $I->see('Déconnexion', '.dashboard-card h5');
+
+        $I->click('DashBoard Administrateur');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeCurrentRouteIs('app_admin_index');
+
+        $I->amOnPage('/dashboard');
+        $I->click('Éditer mon Profil');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeCurrentRouteIs('app_user_edit', ['id' => $admin->getId()]);
+
+        $I->amOnPage('/dashboard');
+        $I->click('Éditer mon Mot de passe');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeCurrentRouteIs('app_user_edit_password', ['id' => $admin->getId()]);
 
         $I->amOnPage('/dashboard');
         $I->click('Déconnexion');
