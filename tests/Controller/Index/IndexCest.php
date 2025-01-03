80def667e3adffb22431768cf22e4d59817d2100
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 14:28:50 +0100

    Ajout du test qui vérifie l'ajout d'un produit au panier depuis la page d'accueil

--- tests/Controller/Index/IndexCest.php
+++ tests/Controller/Index/IndexCest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Controller\Index;
 use App\Factory\BrandFactory;
 use App\Factory\CategoryFactory;
 use App\Factory\ProductFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -27,5 +28,28 @@ class IndexCest
         $recommendedProduct = ProductFactory::last();
         $I->see($recommendedProduct->getName(), '.product-card h5');
         $I->see(number_format($recommendedProduct->getPrice(), 2, ',', ' ').' €', '.product-card .price');
+    }
+
+    public function addToCartFromHomePage(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['email' => 'user@example.com', 'password' => 'password']);
+        $category = CategoryFactory::createOne();
+        $brand = BrandFactory::createOne();
+        $product = ProductFactory::createOne(['isRecommended' => true, 'category' => $category, 'brand' => $brand]);
+
+        $I->amOnPage('/login');
+        $I->fillField('email', $user->getEmail());
+        $I->fillField('password', 'password');
+        $I->click('Connexion');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->amOnPage('/');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->click('//button[contains(., "Ajouter au panier")]');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->amOnPage('/cart');
+        $I->see($product->getName(), 'span');
     }
 }
