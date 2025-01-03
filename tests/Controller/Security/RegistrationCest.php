d9e73b10e018f1ab685ce2da21da8046b16d42e7
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 18:58:10 +0100

    Ajout du test d'inscription avec des données invalides

--- tests/Controller/Security/RegistrationCest.php
+++ tests/Controller/Security/RegistrationCest.php
@@ -23,4 +23,27 @@ class RegistrationCest
         $I->click('S\'inscrire');
         $I->seeResponseCodeIsSuccessful(200);
     }
+
+    public function UserCannotRegisterWithInvalidData(ControllerTester $I): void
+    {
+        $I->amOnPage('/');
+        $I->click('Se connecter');
+        $I->click('Pas encore inscrit ? Créez votre compte ici !');
+        $I->seeCurrentUrlEquals('/registration');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->fillField('Prénom *', 'A');
+        $I->fillField('Nom *', 'B');
+        $I->fillField('Adresse email *', 'invalid-email');
+        $I->fillField('Mot de passe *', 'pass');
+        $I->fillField('Confirmer mot de passe *', 'passw');
+        $I->fillField('Numéro de téléphone *', '0123');
+        $I->click('S\'inscrire');
+
+        $I->seeElement('//div[contains(., "Le prénom ne doit pas être inférieur à 2 caractères.")]');
+        $I->seeElement('//div[contains(., "Le nom ne doit pas être inférieur à 2 caractères.")]');
+        $I->seeElement('//div[contains(., "Les mots de passe ne correspondent pas.")]');
+        $I->seeElement('//div[contains(., "Le numéro de téléphone doit contenir 10 chiffres.")]');
+    }
+
 }
