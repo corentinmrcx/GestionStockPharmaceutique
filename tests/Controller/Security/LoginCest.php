7dfe5079be866510990c5986638238c195ef30eb
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 18:16:23 +0100

    Ajout du test qui vérifie que l'authentification fonctionne correctement

---
+++ tests/Controller/Security/LoginCest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\Controller\Security;
+
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
+
+class LoginCest
+{
+    public function UserCanLoginSuccessfully(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['email' => 'user@example.com', 'password' => 'test']);
+
+        $I->amOnPage('/login');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->fillField('email', $user->getEmail());
+        $I->fillField('password', 'password');
+        $I->click('Connexion');
+        $I->seeResponseCodeIsSuccessful(200);
+    }
+
+}
