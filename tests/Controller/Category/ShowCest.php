3ae4b70458540e01eab25d07976429dc39d3edbd
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 17:13:29 +0100

    Ajout du test qui vérifie la pagination sur les pages de catégories

--- tests/Controller/Category/ShowCest.php
+++ tests/Controller/Category/ShowCest.php
@@ -20,4 +20,26 @@ class ShowCest
         $I->assertCount(10, $I->grabMultiple('.product-card'));
         $I->see($category->getNameCategory());
     }
+
+    public function clickOnNextAndPreviousPaginationLinksForCategoriesWorkCorrectly(ControllerTester $I): void
+    {
+        $category = CategoryFactory::createOne();
+        $brand = BrandFactory::createOne();
+
+        ProductFactory::createMany(20, ['category' => $category, 'brand' => $brand]);
+
+        $I->amOnPage('/category/'.$category->getId().'?page=1');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeNumberOfElements('.product-card', 12);
+
+        $I->click('a[rel="next"]');
+        $I->seeInCurrentUrl('page=2');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeNumberOfElements('.product-card', 8);
+
+        $I->click('a[rel="prev"]');
+        $I->seeInCurrentUrl('page=1');
+        $I->seeResponseCodeIsSuccessful(200);
+        $I->seeNumberOfElements('.product-card', 12);
+    }
 }
