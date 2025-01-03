697f77c5f26563e8a3bf6cfbdd486f0e40c9e49e
Louis Baudat <louis.baudat@etudiant.univ-reims.fr>
2025-01-03 15:27:01 +0100

    Ajout du test qui vérifie que le dashboard fonctionne correctement pour un utilisateur qui à le role "user"

--- tests/Controller/UserDashboard/IndexCest.php
+++ tests/Controller/UserDashboard/IndexCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller\UserDashboard;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -13,4 +14,23 @@ class IndexCest
         $I->see('Connexion', 'h1');
         $I->seeInCurrentUrl('/login');
     }
+
+    public function dashboardDisplaysForRoleUser(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['email' => 'user@example.com', 'password' => 'password', 'roles' => ['ROLE_USER']]);
+
+        $I->amOnPage('/login');
+        $I->fillField('email', $user->getEmail());
+        $I->fillField('password', 'password');
+        $I->click('Connexion');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->amOnPage('/dashboard');
+        $I->seeResponseCodeIsSuccessful(200);
+
+        $I->see('Éditer mon Profil', '.dashboard-card h5');
+        $I->see('Éditer mon Mot de passe', '.dashboard-card h5');
+        $I->see('Déconnexion', '.dashboard-card h5');
+    }
+
 }
